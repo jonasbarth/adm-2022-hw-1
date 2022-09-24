9f42ad3d4566bf3ab74c266286bbaf703595536d
jonasbarth <jb901@exeter.ac.uk>
2022-09-24 19:35:47 +0200

    Add py introduction challenges

---
+++ py-introduction/hello_world.py
@@ -0,0 +1,4 @@
+"""Solves https://www.hackerrank.com/challenges/py-hello-world/problem"""
+
+if __name__ == '__main__':
+    print("Hello, World!")
---
+++ py-introduction/print_function.py
@@ -0,0 +1,8 @@
+"""Solves https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true"""
+if __name__ == '__main__':
+    n = int(input())
+    string = ""
+    for i in range(1, n + 1):
+        string += str(i)
+        
+    print(string)
